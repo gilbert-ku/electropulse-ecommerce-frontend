32f91a9fc17c75706447366e9f82839361b76935
gilbert-ku <gilbert45ku@gmail.com>
2023-12-06 23:30:25 +0300

    add sign up page and adjust login stylings

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -12,34 +12,35 @@ const LogIn = () => {
                     <img className="w-40 h-25 mr-2" src={electrologo} alt="logo" />
    
                 </a>
-                <div className="w-full bg-white rounded-lg shadow dark:border  border-amber-600 md:mt-0 sm:max-w-md xl:p-0 ">
+                <div className="w-full bg-white rounded-lg shadow dark:border  border-amber-500 md:mt-0 sm:max-w-md xl:p-0 ">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
                             Sign in to your account
                         </h1>
                         <form className="space-y-4 md:space-y-6" action="#">
                             <div>
-                                <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                <input type="email" name="email" id="email" className="bg-gray-50 border border-amber-600 text-gray-900 sm:text-sm rounded-lg focus:ring-amber-600 focus:border-amber-900 block w-full p-2.5  dark:border-primary-600 dark:placeholder-gray-400  " placeholder="name@email.com" required="" />
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                <input type="email" name="email" id="email" className="bg-gray-50 border border-amber-500 text-gray-900 sm:text-sm rounded-lg focus:ring-amber-500 focus:border-amber-900 block w-full p-2.5  dark:border-primary-600 dark:placeholder-gray-400  " placeholder="name@email.com" required="" />
                             </div>
                             <div>
-                                <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" className=" border  text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-amber-600 block w-full p-2.5  dark:border-amber-600 dark:placeholder-gray-400    " required="" />
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••" className=" border  text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-amber-500 block w-full p-2.5  dark:border-amber-500 dark:placeholder-gray-400    " required="" />
                             </div>
                             <div className="flex items-center justify-between">
                                 <div className="flex items-start">
                                     <div className="flex items-center h-5">
-                                            <input id="remember" aria-describedby="remember" type="checkbox" className="w-full bg-white rounded-lg border border-amber-600 shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"required="" />
+                                        <input id="remember" aria-describedby="remember" type="checkbox" className="w-full bg-white rounded-lg border border-amber-500 shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"required="" />
                                     </div>
                                     <div className="ml-3 text-sm">
-                                        <label for="remember" className="text-amber-600 dark:text-gray-500">Remember me</label>
+                                        <label  className="text-amber-500 dark:text-gray-500">Remember me</label>
                                     </div>
                                 </div>
                                 <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                             </div>
-                            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                            <p className="text-sm font-light text-gray-500 dark:text-gray-500">
-                                Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                            <button className="bg-transparent w-full hover:bg-amber-500 text-amber-500 font-semibold hover:text-white py-2 px-4 border border-amber-500 hover:border-transparent rounded">
+                                Sign In
+                            </button>                            <p className="text-sm font-light text-gray-500 dark:text-gray-500">
+                                Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-amber-500">Sign up</a>
                             </p>
                         </form>
                     </div>
